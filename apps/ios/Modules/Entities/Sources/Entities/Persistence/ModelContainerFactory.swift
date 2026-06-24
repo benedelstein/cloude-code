feb98fe341c95257248cf5317d0c62910508146f
Ben Edelstein <36255968+benedelstein@users.noreply.github.com>
2026-06-24 12:10:43 -0700

    Remove message cache schema bump

--- apps/ios/Modules/Entities/Sources/Entities/Persistence/ModelContainerFactory.swift
+++ apps/ios/Modules/Entities/Sources/Entities/Persistence/ModelContainerFactory.swift
@@ -4,32 +4,24 @@ public enum SchemaV1: VersionedSchema {
     public static let versionIdentifier = Schema.Version(1, 0, 0)
 
     public static var models: [any PersistentModel.Type] {
-        [UserEntity.self, SessionSummaryEntity.self]
+        [UserEntity.self, SessionSummaryEntity.self, SessionMessageEntity.self]
     }
 
     public static var entities: [any Entity.Type] {
-        [UserEntity.self, SessionSummaryEntity.self]
-    }
-}
-
-public enum SchemaV2: VersionedSchema {
-    public static let versionIdentifier = Schema.Version(1, 1, 0)
-
-    public static var models: [any PersistentModel.Type] {
         [UserEntity.self, SessionSummaryEntity.self, SessionMessageEntity.self]
     }
 }
 
-public typealias CurrentSchema = SchemaV2
+public typealias CurrentSchema = SchemaV1
 
 public enum MigrationPlan: SchemaMigrationPlan {
     public static var schemas: [any VersionedSchema.Type] {
-        [SchemaV1.self, SchemaV2.self]
+        [SchemaV1.self]
     }
 
     // Add a MigrationStage per schema bump (e.g. .lightweight(fromVersion:toVersion:)).
     public static var stages: [MigrationStage] {
-        [.lightweight(fromVersion: SchemaV1.self, toVersion: SchemaV2.self)]
+        []
     }
 }
 
